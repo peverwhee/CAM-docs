a12542f106d57c625701f0d05499d7b0ea031ac5
Rich Neale <rneale@ucar.edu>
2018-05-15 16:28:22 -0600

    CAM5 sci. doc. cleanup

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -35,7 +35,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.imgmath',
     'sphinx.ext.githubpages',
-    'sphinxcontrib.bibtex']
+    'sphinxcontrib.bibtex',
+    'pybtex']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
